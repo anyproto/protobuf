5ce0479484ba453b4e40195cfe9948de94a6f30f
requilence <requilence@gmail.com>
2019-11-07 12:52:11 +0100

    add env var to export oneOf interface

--- protoc-gen-gogo/generator/generator.go
+++ protoc-gen-gogo/generator/generator.go
@@ -74,7 +74,9 @@ import (
 // proto package is introduced; the generated code references
 // a constant, proto.ProtoPackageIsVersionN (where N is generatedCodeVersion).
 const generatedCodeVersion = 3
-const noUnderscoreModeEnvVar = "GOGO_NO_UNDERSCORE"
+const envVarNoUnderscoreMode = "GOGO_NO_UNDERSCORE"
+const envVarExportOneOfInterface = "GOGO_EXPORT_ONEOF_INTERFACE"
+
 // A Plugin provides functionality to add to the output during Go code generation,
 // such as to produce RPC stubs.
 type Plugin interface {
@@ -2881,11 +2883,19 @@ func (g *Generator) generateMessage(message *Descriptor) {
 			// when we've computed any disambiguation.
 
 			var dname string
-			if os.Getenv(noUnderscoreModeEnvVar) == "1" {
-				dname = "is" + goTypeName + fname
+			var interfacePrefix string
+			if os.Getenv(envVarExportOneOfInterface) == "1" {
+				interfacePrefix = "Is"
 			} else {
-				dname = "is" + goTypeName + fname
+				interfacePrefix = "is"
 			}
+
+			if os.Getenv(envVarNoUnderscoreMode) == "1" {
+				dname = interfacePrefix + goTypeName + fname
+			} else {
+				dname = interfacePrefix + goTypeName + fname
+			}
+
 			oneOftag := `protobuf_oneof:"` + odp.GetName() + `"`
 			of := oneofField{
 				fieldCommon: fieldCommon{
@@ -2921,7 +2931,7 @@ func (g *Generator) generateMessage(message *Descriptor) {
 		dvalue := g.getterDefault(field, goTypeName, GoTypeToName(goTyp))
 		if oneof {
 			var tname string
-			if os.Getenv(noUnderscoreModeEnvVar) == "1" {
+			if os.Getenv(envVarNoUnderscoreMode) == "1" {
 				tname = goTypeName + oneOfFieldName + "Of" + fieldName
 			} else {
 				tname = goTypeName + "_" + fieldName
@@ -3362,7 +3372,7 @@ func CamelCase(s string) string {
 // CamelCaseSlice is like CamelCase, but the argument is a slice of strings to
 // be joined with "_".
 func CamelCaseSlice(elem []string) string {
-	if os.Getenv(noUnderscoreModeEnvVar) == "1" {
+	if os.Getenv(envVarNoUnderscoreMode) == "1" {
 		return CamelCase(strings.Join(elem, ""))
 	}
 	return CamelCase(strings.Join(elem, "_"))
